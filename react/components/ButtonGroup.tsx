b65aa0bf1eda25f2bc9ca57ba7eaad6e2c2e7711
Danilo Ibañez <94373834+Danilo1103@users.noreply.github.com>
2023-02-16 22:24:19 -0500

    feat: modify add-to-cart-info

--- react/components/ButtonGroup.tsx
+++ react/components/ButtonGroup.tsx
@@ -4,16 +4,14 @@ import styles from "./styles.css"
 
 const ButtonGroup = ({ blockClass }: { blockClass: string }) => {
     const container__buttons = generateBlockClass(styles.container__buttons, blockClass)
-    const container__buttonCheckout = generateBlockClass(styles.container__buttonCheckout, blockClass)
     const container__buttonCart = generateBlockClass(styles.container__buttonCart, blockClass)
 
     return (
         <div className={container__buttons}>
-            <div className={container__buttonCheckout}>
-                <a href="/checkout/#/cart">IR AL CHECK-OUT</a>
-            </div>
             <div className={container__buttonCart}>
-                <button>VER CARRITO</button>
+                <a href="/checkout/#/cart">
+                    <button >IR AL CHECKOUT</button>
+                </a>
             </div>
         </div>
     )
